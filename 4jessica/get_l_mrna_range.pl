54249ae9b3f151ff19dda8594b4ce6614d134a05
Fengyuan Chen <cfy1990@gmail.com>
2013-03-05 22:31:12 +0800

    fix bug and output prettily

--- 4jessica/get_l_mrna_range.pl
+++ 4jessica/get_l_mrna_range.pl
@@ -34,11 +34,16 @@ sub get_l_mrna_range{
   my @dna_range=get_dna_exon_range($dna_file);
   my $rna_length=get_content($l_mrna_file);
   my $total=0;
+  my @intron_len;
+  # 得到每个intron的长度
+  for (my ($i,$len)=(0,scalar @dna_range);$i<$len - 1;$i++) {
+    push @intron_len,($dna_range[$i+1]->[0]-$dna_range[$i]->[1]);
+  }
   # reverse，这样从后面开始处理
   my @dna_range_reverse=reverse @dna_range;
   # 获取每个段的长度，存到@rna_len_reverse里面，这里会根据rna_length来获得长度，所以，要逆向处理。
   my @rna_len_reverse;
-  foreach(@dna_range_reverse){
+  foreach (@dna_range_reverse) {
     last if $total > $rna_length;
     my $len =$_->[1]-$_->[0];
     $total+=$len;
@@ -48,23 +53,39 @@ sub get_l_mrna_range{
   my @rna_len=reverse @rna_len_reverse;
   my @rna_range;
   my $i=1;
-  foreach(@rna_len){
+  foreach (@rna_len) {
     push @rna_range,"$i..@{[($i+$_)]}";
-    $i+=$_;
+    $i+=$_+1;
   }
-  return @rna_range;
+  return (\@rna_range,\@intron_len);
 }
 
 # 第一个参数:dna文件名
 # 第二个参数:l_mrna文件名
 # 返回结果字符串，调用get_l_mrna_range，然后转换成字符串返回
-sub get_l_mrna_range_to_string{
+sub print_get_l_mrna_range{
   my $dna_file=shift;
   my $l_mrna_file=shift;
-  my $string=join ",",&get_l_mrna_range($dna_file,$l_mrna_file);
-  return $string;
+  my $output_file=shift;
+  my @result=&get_l_mrna_range($dna_file,$l_mrna_file);
+  my @rna_range=@{$result[0]};
+  my @intron_len=@{$result[1]};
+  open my $out,">",$output_file;
+  say $out "exon number :";
+  say $out scalar @rna_range;
+  say $out "exon position :";
+  say $out join ",",@rna_range;
+  say $out "intron number :";
+  say $out scalar @intron_len;
+  say $out "intron size :";
+  say $out join ",",@intron_len;
 }
-
+if(@ARGV<3){
+  say "$0 : dna-file l_mrna_file output_file";
+  exit(1);
+}
 my $dna_file=shift;
 my $l_mrna_file=shift;
-say &get_l_mrna_range_to_string($dna_file,$l_mrna_file);
+my $output_file=shift;
+print_get_l_mrna_range($dna_file,$l_mrna_file,$output_file);
+
